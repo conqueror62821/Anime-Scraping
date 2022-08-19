08c004b3c1f5e2e2ec12a91f65c0142c1d31a7ce
john62821 <johndavid62821@gmail.com>
2022-08-19 12:48:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "api_url": "https://animexninja-api.dhvitop1.repl.co/",
+  "api_url": "http://localhost:6969/",
   // Enter your api host's url or if hosting locally use https://localhost:6969
   
   "time_update": "200",
