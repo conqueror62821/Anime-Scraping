518b898f79896426f4cfd82e7415770651febcdb
john62821 <johndavid62821@gmail.com>
2022-08-24 20:23:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "api_url": "http://localhost:6969/",
-  // Enter your api host's url or if hosting locally use https://localhost:6969
+  "api_url": "https://nothingne.herokuapp.com/",
+  // Enter your api host's url or if hosting locally/on your own computer use https://localhost:80
   
   "time_update": "200",
   // Enter Seconds for updating recently added and popular list. For disabling it Enter disable
